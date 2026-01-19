35f5ff674e28c3f8ddc31e4b3226627f9c6c6ed9
Akhil Kamsala <akamsala@tekion.com>
2026-01-19 23:06:37 +0530

    Fix Babel config: Detect test environment for ES module transformation in CI

--- babel.config.js
+++ babel.config.js
@@ -1,37 +1,42 @@
-module.exports = {
-  presets: [
-    [
-      "@babel/preset-env",
-      {
-        modules: false
-      }
-    ],
-    "@babel/preset-react"
-  ],
-  plugins: [
-    "@babel/plugin-transform-runtime",
-    "@babel/plugin-syntax-dynamic-import",
-    "@babel/plugin-proposal-class-properties"
-  ],
-  env: {
-    production: {
-      only: ["src"],
-      plugins: [
-        [
-          "transform-react-remove-prop-types",
-          {
-            removeImport: true
-          }
-        ],
-        "@babel/plugin-transform-react-inline-elements",
-        "@babel/plugin-transform-react-constant-elements"
-      ]
-    },
-    test: {
-      presets: [
+module.exports = function(api) {
+  // Cache configuration based on NODE_ENV
+  api.cache.using(() => process.env.NODE_ENV);
+
+  const isTest = api.env('test') || process.env.NODE_ENV === 'test' || process.env.JEST_WORKER_ID !== undefined;
+
+  const presets = isTest
+    ? [
         ["@babel/preset-env", { targets: { node: "current" } }],
         "@babel/preset-react"
       ]
+    : [
+        ["@babel/preset-env", { modules: false }],
+        "@babel/preset-react"
+      ];
+
+  const plugins = [
+    "@babel/plugin-transform-runtime",
+    "@babel/plugin-syntax-dynamic-import",
+    "@babel/plugin-proposal-class-properties"
+  ];
+
+  return {
+    presets,
+    plugins,
+    env: {
+      production: {
+        only: ["src"],
+        plugins: [
+          [
+            "transform-react-remove-prop-types",
+            {
+              removeImport: true
+            }
+          ],
+          "@babel/plugin-transform-react-inline-elements",
+          "@babel/plugin-transform-react-constant-elements"
+        ]
+      }
     }
-  }
+  };
 };
